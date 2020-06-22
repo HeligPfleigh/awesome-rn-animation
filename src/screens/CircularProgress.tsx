d467ced7b100c12909d63b6cfb50ac06bee65c1f
goldenhands92 <trantuan1892@gmail.com>
2020-06-22 09:19:48 +0700

    chore: circular progress by svg

--- src/screens/CircularProgress.tsx
+++ src/screens/CircularProgress.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Easing, Clock} from 'react-native-reanimated';
+import {timing} from 'react-native-redash';
 
-export default function CircularProgress() {
+import CircularProgress from '../components/CircularProgress';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function CircularProgressScreen() {
+  const clock = new Clock();
+  const config = {
+    duration: 10 * 1000,
+    toValue: 1,
+    easing: Easing.linear,
+  };
   return (
-    <View>
-      <Text>circular progress</Text>
+    <View style={styles.container}>
+      <CircularProgress progress={timing({clock, ...config})} />
     </View>
   );
 }
